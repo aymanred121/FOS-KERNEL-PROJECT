9ce71f9367ac6544792ea06f67ad6818ae3cd241
Badr Mohamed <badrmohamed@pop-os.localdomain>
2021-12-25 16:56:19 +0200

    modified free()

--- lib/uheap.c
+++ lib/uheap.c
@@ -16,25 +16,25 @@
 //============================ REQUIRED FUNCTIONS ==================================//
 //==================================================================================//
 
-	int UH_UnAllocated_Space_Start=USER_HEAP_START;
-	uint32 num_OF_Free_pages_In_heap = (USER_HEAP_MAX-USER_HEAP_START)/PAGE_SIZE;
-	uint32 allocation_Info[(USER_HEAP_MAX-USER_HEAP_START)/PAGE_SIZE] = {0};
-	uint32 num_OF_pages_In_heap = (USER_HEAP_MAX-USER_HEAP_START)/PAGE_SIZE;
-	int flage = 0;
+int UH_UnAllocated_Space_Start = USER_HEAP_START;
+uint32 num_OF_Free_pages_In_heap = (USER_HEAP_MAX - USER_HEAP_START) / PAGE_SIZE;
+uint32 allocation_Info[(USER_HEAP_MAX - USER_HEAP_START) / PAGE_SIZE] = {0};
+uint32 num_OF_pages_In_heap = (USER_HEAP_MAX - USER_HEAP_START) / PAGE_SIZE;
+int flage = 0;
 // 1 000 1
 
-void* malloc(uint32 size)
+void *malloc(uint32 size)
 {
 	//TODO: [PROJECT 2021 - [2] User Heap] malloc() [User Side]
-//	cprintf("start");
-	size = ROUNDUP(size,PAGE_SIZE);
-	if(size==0 || size > (num_OF_Free_pages_In_heap * PAGE_SIZE))
+	//	cprintf("start");
+	size = ROUNDUP(size, PAGE_SIZE);
+	if (size == 0 || size > (num_OF_Free_pages_In_heap * PAGE_SIZE))
 		return NULL;
 
 	///////******* Searching for space ******////////
-	int min_Satisfying_Frames_Found=-1;
-	uint32 Start_Address_OF_Min_Size=0;
-	for(int i=0 ; i <num_OF_pages_In_heap ;i++)
+	int min_Satisfying_Frames_Found = -1;
+	uint32 Start_Address_OF_Min_Size = 0;
+	for (int i = 0; i < num_OF_pages_In_heap; i++)
 	{
 		// 512kb
 		// i = 0, j = 0,allocate = 3mb, uh-un = 8060 0000
@@ -50,93 +50,84 @@ void* malloc(uint32 size)
 		 *0      3 800
 		 * */
 
-		if(allocation_Info[i]==0)
+		if (allocation_Info[i] == 0)
 		{
-			int counter=0;
-			for(int j = i ; j < num_OF_pages_In_heap ; j++ )
+			int counter = 0;
+			for (int j = i; j < num_OF_pages_In_heap; j++)
 			{
-				if((j*PAGE_SIZE)+USER_HEAP_START== (UH_UnAllocated_Space_Start) || ((j-1)*PAGE_SIZE)+USER_HEAP_START == (UH_UnAllocated_Space_Start) )
+				if ((j * PAGE_SIZE) + USER_HEAP_START == (UH_UnAllocated_Space_Start) || ((j - 1) * PAGE_SIZE) + USER_HEAP_START == (UH_UnAllocated_Space_Start))
 				{
-					if(size < USER_HEAP_MAX-UH_UnAllocated_Space_Start)
+					if (size < USER_HEAP_MAX - UH_UnAllocated_Space_Start)
 					{
-						if((USER_HEAP_MAX-UH_UnAllocated_Space_Start)/PAGE_SIZE <min_Satisfying_Frames_Found
-							|| min_Satisfying_Frames_Found ==-1)
+						if ((USER_HEAP_MAX - UH_UnAllocated_Space_Start) / PAGE_SIZE < min_Satisfying_Frames_Found || min_Satisfying_Frames_Found == -1)
 						{
-							uint32 final_=(UH_UnAllocated_Space_Start-USER_HEAP_START)/PAGE_SIZE;
-							 sys_allocateMem(UH_UnAllocated_Space_Start,size);
-							allocation_Info[final_]=size/PAGE_SIZE;
-							UH_UnAllocated_Space_Start+=size;
-							num_OF_Free_pages_In_heap-=size/PAGE_SIZE;
-							return (void *)(UH_UnAllocated_Space_Start-size);
-
+							uint32 final_ = (UH_UnAllocated_Space_Start - USER_HEAP_START) / PAGE_SIZE;
+							sys_allocateMem(UH_UnAllocated_Space_Start, size);
+							allocation_Info[final_] = size / PAGE_SIZE;
+							UH_UnAllocated_Space_Start += size;
+							num_OF_Free_pages_In_heap -= size / PAGE_SIZE;
+							return (void *)(UH_UnAllocated_Space_Start - size);
 						}
 						else
 						{
-							uint32 final_va=(Start_Address_OF_Min_Size*PAGE_SIZE)+USER_HEAP_START;
-							sys_allocateMem(final_va,size);
-							allocation_Info[Start_Address_OF_Min_Size]=size/PAGE_SIZE;
-							num_OF_Free_pages_In_heap-=size/PAGE_SIZE;
+							uint32 final_va = (Start_Address_OF_Min_Size * PAGE_SIZE) + USER_HEAP_START;
+							sys_allocateMem(final_va, size);
+							allocation_Info[Start_Address_OF_Min_Size] = size / PAGE_SIZE;
+							num_OF_Free_pages_In_heap -= size / PAGE_SIZE;
 							return (void *)final_va;
 						}
 					}
 				}
-				else if(allocation_Info[j]==0)
+				else if (allocation_Info[j] == 0)
 				{
-                    counter++;
-					if(j == num_OF_pages_In_heap-1 )
-					   {
+					counter++;
+					if (j == num_OF_pages_In_heap - 1)
+					{
 						//cprintf("  %d , %d , %d \n" , i , counter , size/PAGE_SIZE);
-						if(size/PAGE_SIZE <= counter)
+						if (size / PAGE_SIZE <= counter)
 						{
-							if(counter <min_Satisfying_Frames_Found
-							|| min_Satisfying_Frames_Found ==-1)
+							if (counter < min_Satisfying_Frames_Found || min_Satisfying_Frames_Found == -1)
 							{
-								min_Satisfying_Frames_Found=counter;
-								Start_Address_OF_Min_Size=i;
-
+								min_Satisfying_Frames_Found = counter;
+								Start_Address_OF_Min_Size = i;
 							}
 						}
 
-					     i=num_OF_pages_In_heap+5;
-					          break;
-					    }
-
+						i = num_OF_pages_In_heap + 5;
+						break;
+					}
 				}
 
 				else
 				{
-					if(size/PAGE_SIZE <= counter)
+					if (size / PAGE_SIZE <= counter)
 					{
-						if(counter <min_Satisfying_Frames_Found
-								|| min_Satisfying_Frames_Found ==-1)
+						if (counter < min_Satisfying_Frames_Found || min_Satisfying_Frames_Found == -1)
 						{
-							min_Satisfying_Frames_Found=counter;
-							Start_Address_OF_Min_Size=i;
-
+							min_Satisfying_Frames_Found = counter;
+							Start_Address_OF_Min_Size = i;
 						}
 					}
-					i=j-1;
+					i = j - 1;
 					break;
 				}
 			}
 		}
 		else
 		{
-            i+=allocation_Info[i]-1;
+			i += allocation_Info[i] - 1;
 		}
 	}
-	if(min_Satisfying_Frames_Found==-1)
+	if (min_Satisfying_Frames_Found == -1)
 		return NULL;
 	else
 	{
-		uint32 final_va=(Start_Address_OF_Min_Size*PAGE_SIZE)+USER_HEAP_START;
-	    sys_allocateMem(final_va,size);
-	    allocation_Info[Start_Address_OF_Min_Size]=size/PAGE_SIZE;
-	    num_OF_Free_pages_In_heap-=size/PAGE_SIZE;
-	    return (void *)final_va;
-
+		uint32 final_va = (Start_Address_OF_Min_Size * PAGE_SIZE) + USER_HEAP_START;
+		sys_allocateMem(final_va, size);
+		allocation_Info[Start_Address_OF_Min_Size] = size / PAGE_SIZE;
+		num_OF_Free_pages_In_heap -= size / PAGE_SIZE;
+		return (void *)final_va;
 	}
-
 }
 
 // free():
@@ -149,32 +140,34 @@ void* malloc(uint32 size)
 //		"memory_manager.c", then switch back to the user mode here
 //	the freeMem function is empty, make sure to implement it.
 
-void free(void* virtual_address)
+void free(void *virtual_address)
 {
 	//TODO: [PROJECT 2021 - [2] User Heap] free() [User Side]
 	flage = 1;
-	uint32 index_in_the_array=((uint32)(virtual_address)-USER_HEAP_START)/PAGE_SIZE;
-	sys_freeMem((uint32)virtual_address,allocation_Info[index_in_the_array]*PAGE_SIZE);
-	allocation_Info[index_in_the_array]=0;
+	uint32 index_in_the_array = ((uint32)(virtual_address)-USER_HEAP_START) / PAGE_SIZE;
+	sys_freeMem((uint32)virtual_address, allocation_Info[index_in_the_array] * PAGE_SIZE);
+	if ((uint32)virtual_address + allocation_Info[index_in_the_array] * PAGE_SIZE == UH_UnAllocated_Space_Start)
+		UH_UnAllocated_Space_Start = (uint32)virtual_address;
+	allocation_Info[index_in_the_array] = 0;
 }
 
 //==================================================================================//
 //================================ OTHER FUNCTIONS =================================//
 //==================================================================================//
 
-void* smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
+void *smalloc(char *sharedVarName, uint32 size, uint8 isWritable)
 {
 	panic("this function is not required...!!");
 	return 0;
 }
 
-void* sget(int32 ownerEnvID, char *sharedVarName)
+void *sget(int32 ownerEnvID, char *sharedVarName)
 {
 	panic("this function is not required...!!");
 	return 0;
 }
 
-void sfree(void* virtual_address)
+void sfree(void *virtual_address)
 {
 	panic("this function is not required...!!");
 }
@@ -194,7 +187,7 @@ void shrink(uint32 newSize)
 	panic("this function is not required...!!");
 }
 
-void freeHeap(void* virtual_address)
+void freeHeap(void *virtual_address)
 {
 	panic("this function is not required...!!");
 }
